d6ecd6f18e141c52d651494b3f6a249955de98bc
Naman Taneja <42579074+namantaneja167@users.noreply.github.com>
2021-08-21 00:16:14 +0530

    Add files via upload

---
+++ src/components/ChakraButton/ChakraButton.stories.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import { Button } from '@chakra-ui/button';
+
+
+export default {
+    title: 'Chakra/Button',
+    component: Button
+}
+
+export const Success = ()=> <Button variantColor="green">Success</Button>
+export const Danger = ()=> <Button variantColor="red">Danger</Button>
